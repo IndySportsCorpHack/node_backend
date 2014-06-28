46b420c36542e22e32e381221187cbc54e31e04d
Michael Price <mtnp55@live.com>
2014-06-28 13:03:14 -0400

    TROLOLOLOL LOLOL LOLOL

--- backend/public/javascripts/global.js
+++ backend/public/javascripts/global.js
@@ -5,15 +5,17 @@ var volunteerListData = [];
 $(document).ready(function() {
 
     // Populate the user table on initial page load
-    populateTable();
+    populateVolunteerTable();
     $('#volunteerList table tbody').on('click', 'td a.linkshowvolunteer', showVolunterInfo);
+    $('#btnAddVolunteer').on('click', createVolunteer);
+    $('#volunteerList table tbody').on('click', 'td a.linkdeletevolunteer', deleteVolunteer);
 
 });
 
 // Functions =============================================================
 
 // Fill table with data
-function populateTable() {
+function populateVolunteerTable() {
 
     // Empty content string
     var tableContent = '';
@@ -34,6 +36,7 @@ function populateTable() {
             tableContent += '<td>' + this.age + '</td>';
             tableContent += '<td>' + this.city + '</td>';
             tableContent += '<td>' + this.state + '</td>';
+            tableContent += '<td>' + this.zip + '</td>';
             tableContent += '<td>' + this.event_type + '</td>';
             tableContent += '<td><a href="#" class="linkdeletevolunteer" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
@@ -60,8 +63,111 @@ function showVolunterInfo(event) {
     var thisVolunteerObject = volunteerListData[arrayPosition];
 
     //Populate Info Box
+    $('#volunteerInfoEmail').text(thisVolunteerObject.email);
     $('#volunteerInfoFirstName').text(thisVolunteerObject.first_name);
     $('#volunteerInfoLastName').text(thisVolunteerObject.last_name);
     $('#volunteerInfoAge').text(thisVolunteerObject.age);
+    $('#volunteerInfoCity').text(thisVolunteerObject.city);
+    $('#volunteerInfoState').text(thisVolunteerObject.state);
+    $('#volunteerInfoZip').text(thisVolunteerObject.zip);
+    $('#volunteerInfoEventType').text(thisVolunteerObject.event_type);
+
+};
+
+// Add Volunteer
+function createVolunteer(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addVolunteer input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newVolunteer = {
+            'email': $('#addVolunteer fieldset input#inputVolunteerEmail').val(),
+            'first_name': $('#addVolunteer fieldset input#inputVolunteerFirstName').val(),
+            'last_name': $('#addVolunteer fieldset input#inputVolunteerLastName').val(),
+            'mobile_phone': $('#addVolunteer fieldset input#inputVolunteerMobilePhone').val(),
+            'age': $('#addVolunteer fieldset input#inputVolunteerAge').val(),
+            'city': $('#addVolunteer fieldset input#inputVolunteerCity').val(),
+            'state': $('#addVolunteer fieldset input#inputVolunteerState').val(),
+            'zip': $('#addVolunteer fieldset input#inputVolunteerZip').val(),
+            'event_type': $('#addVolunteer fieldset input#inputVolunteerEventType').val(),
+        }
+
+        // Use AJAX to post the object to our createvolunteer service
+        $.ajax({
+            type: 'POST',
+            data: newVolunteer,
+            url: '/volunteers/createvolunteer',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === 'Successfully created volunteer') {
+
+                // Clear the form inputs
+                $('#addVolunteer fieldset input').val('');
+
+                // Update the table
+                populateVolunteerTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+// Delete User
+function deleteVolunteer(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this volunteer?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/volunteers/deletevolunteer/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === 'Successfully deleted volunteer') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateVolunteerTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
 
 };
--- backend/routes/volunteers.js
+++ backend/routes/volunteers.js
@@ -11,6 +11,28 @@ router.get('/volunteerlist', function(req, res) {
     });
 });
 
+/*
+ * POST to addvolunteer.
+ */
+router.post('/createvolunteer', function(req, res) {
+    var db = req.db;
+    db.collection('volunteercollection').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: 'Successfully created volunteer' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deletevolunteer/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('volunteercollection').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: 'Successfully deleted volunteer' } : { msg:'error: ' + err });
+    });
+});
 
 
 
@@ -18,6 +40,11 @@ router.get('/volunteerlist', function(req, res) {
 /* GET volunteer list page. */
 router.get('/list', function(req, res) {
     res.render('volunteers/list', { layout: 'layout.jade', title: 'Hello, World!' })
+});
+
+/* GET volunteer home page. */
+router.get('/', function(req, res) {
+    res.render('volunteers/index', {})
 });
 
 module.exports = router;
